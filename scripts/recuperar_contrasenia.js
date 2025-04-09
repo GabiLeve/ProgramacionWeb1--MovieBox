2b670fb5db88737d1ad4a12ba6f433d9a842f190
Gabriela <leverattomariag@gmail.com>
2025-04-08 21:43:56 -0300

    Refactor: recover pass button 'cancelar'

--- scripts/recuperar_contrasenia.js
+++ scripts/recuperar_contrasenia.js
@@ -17,14 +17,12 @@
     }
 });
 
-
 document.addEventListener("DOMContentLoaded", () => {
     const form = document.getElementById("form_recuperar_contrasenia");
     const emailInput = document.getElementById("email_recuperar_contrasenia");
     const userInput = document.getElementById("usuario_recuperar_contrasenia");
     const enviarBtn = document.getElementById("enviar_recuperar_contrasenia");
    
-  
     form.addEventListener("submit", (e) => {
       e.preventDefault(); 
   
@@ -36,14 +34,17 @@ document.addEventListener("DOMContentLoaded", () => {
       let errores = [];  
 
       if (!emailValido) {
-        errores.push("El e-mail no es válido"); 
+        errores.push("El email es incorrecto"); 
       }
   
       if (!passValida) {
-        errores.push("La contraseña debe tener mínimo 6 dígitos");
+        errores.push("La contraseña es incorrecta");
       }
-      if (errores.length > 0) {
+      if (errores.length == 1) {
         alert(errores.join("\n")); 
+      }
+      if (errores.length == 2) {
+        alert("Los datos ingresados son incorrectos"); 
       }
       if (errores.length == 0){
       alert("Verifique su bandeja de entrada para recuperar la contraseña");
@@ -51,5 +52,14 @@ document.addEventListener("DOMContentLoaded", () => {
     }
       });
   });
+
+  document.addEventListener('DOMContentLoaded', function() {
+    const botonCancelar = document.querySelector('.boton_recu_contraseña:not(#enviar_recuperar_contrasenia)');
+    
+    botonCancelar.addEventListener('click', function() {
+        
+        window.location.href = "../index.html";
+    });
+});
 
 
